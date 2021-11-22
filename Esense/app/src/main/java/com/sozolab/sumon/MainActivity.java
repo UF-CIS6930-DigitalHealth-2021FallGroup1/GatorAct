31878580e57114097af3c0b3a212018851d0b0ef
Yi-Ming Chang <69064626+yimingStar@users.noreply.github.com>
2021-11-22 16:26:36 -0500

    adding firebase config and firestore

--- Esense/app/src/main/java/com/sozolab/sumon/MainActivity.java
+++ Esense/app/src/main/java/com/sozolab/sumon/MainActivity.java
@@ -9,11 +9,11 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.SystemClock;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -32,7 +32,6 @@ import com.sozolab.sumon.io.esense.esenselib.ESenseManager;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
